46dbf009a8a06b8499340e5134c269a091830f76
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-08-10 18:27:21 +0530

    test-1

--- app/main.js
+++ app/main.js
@@ -5,6 +5,7 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     socket.end();
   });
